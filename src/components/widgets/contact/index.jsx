5910d6b58ca3e0d2ca3f8e460b0e5b6999ddaada
whiwf <leetusoanh@gmail.com>
2022-01-02 13:26:53 +0700

    Refactor widgets

--- src/components/widgets/contact/index.jsx
+++ src/components/widgets/contact/index.jsx
@@ -12,42 +12,76 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectorUser } from "slice/userSlice";
 import "./contact.css";
+import { Person } from "@mui/icons-material";
 
 function Contact() {
   const user = useSelector(selectorUser);
 
-  const [activeUsers, setActiveUsers] = useState([]);
+  const [credentialUsers, setCredentialUsers] = useState([]);
+  const [onlineUsers, setOnlineUsers] = useState([]);
+  const [offlineUsers, setOfflineUsers] = useState([]);
 
   // load active user
   useEffect(() => {
     let unsubscribe = db.collection("users").onSnapshot((snapshot) => {
       let actives = snapshot.docs.map((doc) => doc.data());
-      setActiveUsers(actives.filter((active) => active.uid !== user.uid));
+      setCredentialUsers(actives.filter((active) => active.uid !== user.uid));
     });
 
     return () => unsubscribe(); // stop listening to change
   }, [user.uid]);
 
+  // filter online user
+  useEffect(() => {
+    setOnlineUsers(credentialUsers.filter((user) => user.online));
+  }, [credentialUsers]);
+  // filter offline user
+  useEffect(() => {
+    setOfflineUsers(credentialUsers.filter((user) => !user.online));
+  }, [credentialUsers]);
+
+  const renderListUsers = (arrayUsers) =>
+    arrayUsers.map((item) => (
+      <ListItemButton key={item.uid}>
+        <ListItemAvatar>
+          <Badge
+            overlap="circular"
+            anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+            variant="dot"
+          >
+            <Avatar alt={item.name} src={item.profileSrc} />
+            <div
+              className={
+                item.online
+                  ? `active-user badget-active`
+                  : `inactive-user badget-active`
+              }
+            ></div>
+          </Badge>
+        </ListItemAvatar>
+        <ListItemText primary={item.name} />
+      </ListItemButton>
+    ));
+
   return (
     <div className="contact">
-      <h3 className="contact__title">{activeUsers.length} người liên hệ</h3>
+      <div className="contact__header">
+        <h3 className="contact__title">Người liên hệ</h3>
+        <div className="contact__title__detail">
+          <div className="contact__title__item">
+            <Person style={{ color: "#41B35D" }} />{" "}
+            <span>{onlineUsers.length}</span>
+          </div>
+          <div className="contact__title__item">
+            <Person style={{ color: "red" }} />{" "}
+            <span>{offlineUsers.length}</span>
+          </div>
+        </div>
+      </div>
       <Box>
         <List aria-label="contact">
-          {activeUsers.map((activeUser) => (
-            <ListItemButton key={activeUser.uid}>
-              <ListItemAvatar>
-                <Badge
-                  overlap="circular"
-                  anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
-                  variant="dot"
-                >
-                  <Avatar alt={activeUser.name} src={activeUser.profileSrc} />
-                  <div className={activeUser.online ? `active-user badget-active` : `inactive-user badget-active`}></div>
-                </Badge>
-              </ListItemAvatar>
-              <ListItemText primary={activeUser.name} />
-            </ListItemButton>
-          ))}
+          {renderListUsers(onlineUsers)}
+          {renderListUsers(offlineUsers)}
         </List>
       </Box>
     </div>
