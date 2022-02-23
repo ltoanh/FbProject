a072036f49825b7271f08d7bd1d969f4b6a918d0
whiwf <leetusoanh@gmail.com>
2022-02-24 01:07:45 +0700

    Config .env

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -2,12 +2,12 @@ import firebase from "firebase";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAwg70-Cz2U0u0MHt1H541DlKFz9FUiDnQ",
-  authDomain: "fb-clone-12e42.firebaseapp.com",
-  projectId: "fb-clone-12e42",
-  storageBucket: "fb-clone-12e42.appspot.com",
-  messagingSenderId: "780819680369",
-  appId: "1:780819680369:web:1f290aa5e6eaf196909fd0",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECTID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
